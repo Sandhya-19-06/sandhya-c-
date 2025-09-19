b8c27dd9a37dbb18f54fe2509337da7f2ff56fa4
Sandhya <sandhyamandal2706@gmail.com>
2025-09-19 10:41:11 +0530

    initial commit

---
+++ shapes/shapes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+class Shape
+{
+    public virtual void draw() => Console.WriteLine("drawing a shape..");
+    public virtual void erase() => Console.WriteLine("earsing a shape");
+}
+class circle:Shape
+{
+    public override void draw() => Console.WriteLine("drawing a shape..");
+    public override void erase() => Console.WriteLine("earsing a shape");
+}
+class triangle : Shape
+{
+    public override void draw() => Console.WriteLine("drawing a shape..");
+    public override void erase() => Console.WriteLine("earsing a shape");
+}
+class square:Shape
+{
+    public override void draw() => Console.WriteLine("drawing a shape..");
+    public override void erase() => Console.WriteLine("earsing a shape");
+}
+class program
+{
+    static void Main()
+    {
+
+
+        Shape s;
+        s = new circle();
+        s.draw();
+        s.erase();
+        s = new triangle();
+        s.draw();
+        s.erase();
+        s = new square();
+        s.draw();
+        s.erase();
+
+    }
+}
