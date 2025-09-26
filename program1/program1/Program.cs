b8d92869de2b8575eb6efa045c4f5892a53e595b
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:03:45 +0530

    Add files via upload

---
+++ program1/program1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Myclass
+{
+    private int Age;
+        public void showage()
+    {
+        Console.WriteLine("enter the age:");
+        int age = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("AGE IS " +age);
+    }
+
+}
+class program
+{
+    static void Main()
+    {
+        Myclass obj = new Myclass();
+        obj.showage();
+    }
+}
