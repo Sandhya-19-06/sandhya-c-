3350f822dbc2db08a7ebd52c29225ca5de1c8070
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:12:51 +0530

    Add files via upload

---
+++ Filehandling2/Filehandling2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+class program
+{
+    static void Main()
+    {
+        using(StreamWriter writer =new StreamWriter("data.txt"))
+        {
+            writer.WriteLine("Name : Laxmi");
+            writer.WriteLine("course : BCA");
+
+        }
+        using(StreamReader reader=new StreamReader("data.txt"))
+        {
+            String line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                Console.WriteLine("File" + line);
+            }
+        }
+    }
+}
