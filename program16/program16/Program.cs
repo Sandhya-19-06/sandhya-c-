6b435b3c55950031d3695918b1fe050c08207849
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:08:13 +0530

    Add files via upload

---
+++ program16/program16/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+interface Ipayment
+{
+    void makepayment(double amount);
+    
+}
+class creaditcardpayment : Ipayment
+{
+    public void makepayment(double amount)
+    {
+        Console.WriteLine($"payment of {amount:c}  made using creaditcard");
+    }
+    
+}
+class paypalpayment : Ipayment
+{
+    public void makepayment(double amount)
+    {
+        Console.WriteLine($"payment of {amount:c} made using paypal");
+    }
+}
+class upipayment : Ipayment
+{
+    public void makepayment(double amount)
+    {
+        Console.WriteLine($"payment of {amount:c} made using upi");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        Ipayment creaditcard = new creaditcardpayment();
+        Ipayment paypal = new paypalpayment();
+        Ipayment upi = new upipayment();
+        creaditcard.makepayment(10000);
+        paypal.makepayment(20000);
+        upi.makepayment(30000);
+
+    }
+}
