e9c8bba26a9ced44c8a57274007223643f11478a
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:10:24 +0530

    Add files via upload

---
+++ PROGRAM18/PROGRAM18/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+interface camera
+{
+    void capture();
+
+}
+interface printer
+{
+    void print();
+
+}
+class advance : camera , printer
+{
+    public void capture()
+    {
+        Console.WriteLine("capture the photo");
+    }
+    public void print()
+    {
+        Console.WriteLine("print the photo");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        advance a = new advance();
+        a.capture();
+        a.print();
+    }
+}
