4afebad41d4f52159be48e751dbf7334fe2f7b96
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:02:12 +0530

    Add files via upload

---
+++ prog11/prog11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+class system;
+class program1
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the number");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int lastdigit = n % 10;
+        int firstdigit = n;
+        while (firstdigit >= 10)
+        {
+            firstdigit /= 10;
+        }
+        Console.WriteLine("firstdigit: " + firstdigit);
+        Console.WriteLine("lastdigit: "  + lastdigit);
+    }
+}
