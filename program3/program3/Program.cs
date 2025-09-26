42957c1266fc56e8f7312713ea88be36be0933ee
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:06:11 +0530

    Add files via upload

---
+++ program3/program3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class arthermati_cal
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the one number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(a);
+        Console.WriteLine("enter the second number:");
+        int b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(b);
+        Console.WriteLine("enter the operator:+,-,*,/,%");
+        char ope = Convert.ToChar(Console.ReadLine());
+        Console.WriteLine(ope);
+        switch (ope)
+        {
+            case '+':
+
+        }
+       
+
+    }
+
+    
+
+
+
+}
+
