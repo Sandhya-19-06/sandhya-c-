81166591926b0f56f23c75415ffee93b431471a7
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:06:48 +0530

    Add files via upload

---
+++ PROGRAM14/PROGRAM14/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+interface Resizable
+{
+    void Resizewidth(int width);
+    void Resizeheight(int height);
+
+}
+class Rectangle : Resizable
+{
+    private int width;
+    private int height;
+}
+public Rectangle (int width,int height)
+{
+    this.width = width;
+    this.height = height;
+
+}
+public void Resizewidth(int width)
+{
+    this.width = width;
+    Console.WriteLine("resized width to:" + width);
+}
+public void Resizeheight(int height)
+{
+    this.height = height;
+    Console.WriteLine("resized height to :" + height);
+}
+public void display()
+{
+    Console.WriteLine("rectangle[width:" + width + ",height:" + height + "]");
+}
+}
+class program
+{
+    static void Main()
+    {
+        Rectangle r = new Rectangle(10, 20);
+        r.display();
+        r.Resizewidth(30);
+        r.Resizeheight(40);
+        r.display();
+    }
+}
