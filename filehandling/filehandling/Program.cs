85c929a6e1171355523328ef7f3ed67e1e2935fb
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 09:50:30 +0530

    initial commit

---
+++ filehandling/filehandling/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+class program
+{
+    static void Main()
+    {
+        File.WriteAllText("notes.txt", "File Handling in c# is easy ! \n lets read this file next.");
+        String content = File.ReadAllText("notes.txt");
+        Console.WriteLine("File content:\n" + content);
+    }
+}
