2b3bf4e0d6b4c99abfbf82aaadef8aa5fad4dbbd
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:05:21 +0530

    Add files via upload

---
+++ PROGRAM2/PROGRAM2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class animal
+{
+    protected void makesound()
+    {
+        Console.WriteLine("some sound");
+
+    }
+}
+class dog : animal
+{
+    public void bark()
+    {
+        makesound();
+        Console.WriteLine("dog is barking");
+    }
+    
+}
+class prog
+{
+    static void Main()
+    {
+        dog d = new dog();
+        d.bark();
+    }
+}
