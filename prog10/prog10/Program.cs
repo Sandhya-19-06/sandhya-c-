509b409c87615228d89cd8dc459cd9d354365a49
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:01:28 +0530

    Add files via upload

---
+++ prog10/prog10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the string");
+        string str = Convert.ToString(Console.ReadLine());
+        string str1 = str.Substring(0, 6);
+        Console.WriteLine(str1);
+
+    }
+}
