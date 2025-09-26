323b3c38aed469bd3b1e121ecfd42adeb37d5673
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 09:59:02 +0530

    Add files via upload

---
+++ prog7/prog7/Program.cs
@@ -0,0 +1,34 @@
+﻿class car
+{
+    public void enginee()
+    {
+        Console.WriteLine("enginee started");
+
+    }
+}
+class honda : car
+{
+    public void brand()
+    {
+        Console.WriteLine("name of the brand is honda");
+  
+    }
+}
+class kia : car
+{
+    public void company()
+    {
+        Console.WriteLine("name of the company");
+    }
+}
+class program
+{
+    static void Main() 
+    {
+        kia obj = new kia();
+        honda obj1 = new honda();
+        obj.enginee();
+        obj1.brand();
+        obj.company();
+    }
+}
