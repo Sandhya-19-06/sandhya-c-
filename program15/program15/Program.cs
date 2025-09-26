86c100e74fbabe246e3c328c7ab502a800381a78
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:07:29 +0530

    Add files via upload

---
+++ program15/program15/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter a sentence:");
+        string s = Convert.ToString(Console.ReadLine());
+        string[] words = s.Split(' ');
+        Array.Reverse(words);
+        Console.WriteLine(string.Join(" ", words));
+
+    }
+}
+
+
+
