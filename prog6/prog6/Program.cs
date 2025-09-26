b96f689e7127b43b431558d850a84d4f38d56e67
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 09:58:07 +0530

    Add files via upload

---
+++ prog6/prog6/Program.cs
@@ -0,0 +1,31 @@
+﻿class institute
+{
+    public void manage()
+    {
+        Console.WriteLine("institute is managing the student data");
+    }
+}
+class faculty : institute
+{
+    public void teach()
+    {
+        Console.WriteLine("faculties are teaching");
+    }
+}
+class student : faculty
+{
+    public void learn()
+    {
+        Console.WriteLine("students are learning");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        student obj = new student();
+        obj.manage();
+        obj.teach();
+        obj.learn();
+    }
+}
