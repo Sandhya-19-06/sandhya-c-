3ad4a9c61143933f0070116c9282c0c6a31315c8
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:09:05 +0530

    Add files via upload

---
+++ program17/program17/Program.cs
@@ -0,0 +1,32 @@
+﻿class program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.WriteLine("enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = num1 / num2;
+            Console.WriteLine("result:" + result);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine("error : cannot be divided by zero");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine(" cannot divide");
+
+        }
+        finally
+        {
+            Console.WriteLine("CODE EXECUTED");
+        }
+
+
+
+
+}
+}
