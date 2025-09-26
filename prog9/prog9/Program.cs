0047d35e22eeee4ef9650a26a5b20bae345a7845
Sandhya <sandhyamandal2706@gmail.com>
2025-09-26 10:00:37 +0530

    Add files via upload

---
+++ prog9/prog9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class sum_of_digits
+{
+   static void Main()
+    {
+        Console.WriteLine("enter the digits:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        
+        int sum = 0, rem;
+        while (num > 0)
+        {
+            rem = num % 10;
+            sum += rem;
+            num /= 10;
+        }
+        Console.WriteLine("sum of digit is "  +  sum);
+    }
+}
