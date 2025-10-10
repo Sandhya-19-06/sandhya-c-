91008589fdec7ca580df25ba6de34c89fc2da998
Sandhya <sandhyamandal2706@gmail.com>
2025-10-10 10:30:01 +0530

    initial commit

---
+++ delegate/delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+delegate void Greet();
+class program
+{
+    static void sayHello()
+    {
+        Console.WriteLine("Hello, Student");
+
+    }
+    static void Main()
+    {
+        Greet greetdelegate = sayHello;
+        greetdelegate();
+    }
+}
