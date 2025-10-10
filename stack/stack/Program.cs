b32b79a5e85648976de0e92d16481e7855424715
Sandhya <sandhyamandal2706@gmail.com>
2025-10-10 10:14:41 +0530

    initial commit

---
+++ stack/stack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Stack<string> Stack = new Stack<string>();
+        Stack.Push("first");
+        Stack.Push("second");
+        Stack.Push("third");
+        Console.WriteLine(Stack.Pop());
+    }
+}
