c15e7a0ddd72dd16d72cdaa21566c703ff00a7d0
Sandhya <sandhyamandal2706@gmail.com>
2025-10-10 10:15:32 +0530

    initial commit

---
+++ queue/queue/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Queue<string> queue = new Queue<string>();
+        queue.Enqueue("first");
+        queue.Enqueue("second");
+        Console.WriteLine(queue.Dequeue());
+    }
+}
