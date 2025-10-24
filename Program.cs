f0a816b023c072ed59697c1e11bcaf22b24f132d
Sandhya <sandhyamandal2706@gmail.com>
2025-10-24 09:54:46 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class arthermati_cal
+{
+    static void Main()
+    {
+        Console.WriteLine("enter the one number:");
+        int a = Convert.ToInt32(Console.ReadLine());
+       
+        Console.WriteLine("enter the second number:");
+        int b = Convert.ToInt32(Console.ReadLine());
+       
+        Console.WriteLine("enter the operator:+,-,*,/,%");
+        char ope = Convert.ToChar(Console.ReadLine());
+        
+        int result=0;
+        switch (ope)
+        {
+            case '+':
+                result = a + b;
+                break;
+            case '-' :
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            case '%':
+                result = a % b;
+                break;
+
+
+
+
+        }
+        Console.WriteLine(result);
+       
+
+    }
+
+    
+
+
+
+}
+
